e632664afa7d3e90b97321e0f59420003b10e638
jpb <jakub.baranski.33@gmail.com>
2023-10-22 11:06:05 +0100

    Add image and link insertion

---
+++ UltraTextEdit/Dialogs/iOd.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace UltraTextEdit.Dialogs;
+/// <summary>
+/// An empty page that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class iOd : ContentDialog
+{
+
+    public double DefaultWidth
+    {
+        get; set;
+    }
+    public double DefaultHeight
+    {
+        get; set;
+    }
+    public string Tag
+    {
+        get; private set;
+    }
+
+    public iOd()
+    {
+        this.InitializeComponent();
+        Loaded += ImageOptionsDialog_Loaded;
+    }
+
+    private void ImageOptionsDialog_Loaded(object sender, RoutedEventArgs e)
+    {
+        WidthBox.Value = DefaultWidth;
+        HeightBox.Value = DefaultHeight;
+    }
+
+    private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+    {
+        DefaultWidth = WidthBox.Value;
+        DefaultHeight = HeightBox.Value;
+    }
+
+    private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+    {
+    }
+}
--- UltraTextEdit/Views/MainPage.xaml.cs
+++ UltraTextEdit/Views/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using WinRT;
 using Microsoft.Graphics.Canvas.Text;
 using Windows.UI;
 using Microsoft.UI.Xaml.Media;
+using UltraTextEdit.Dialogs;
+using Microsoft.UI.Xaml.Markup;
 
 namespace UltraTextEdit.Views;
 
@@ -438,5 +440,50 @@ public sealed partial class MainPage : Page
 
 
         window.Activate();
+    }
+
+    private async void AddImageButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Open an image file.
+        FileOpenPicker open = new();
+        open.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+        open.FileTypeFilter.Add(".png");
+        open.FileTypeFilter.Add(".jpg");
+        open.FileTypeFilter.Add(".jpeg");
+
+        StorageFile file = await open.PickSingleFileAsync();
+
+        if (file != null)
+        {
+            using IRandomAccessStream randAccStream = await file.OpenAsync(FileAccessMode.Read);
+            var properties = await file.Properties.GetImagePropertiesAsync();
+            int width = (int)properties.Width;
+            int height = (int)properties.Height;
+
+            iOd dialog = new()
+            {
+                DefaultWidth = width,
+                DefaultHeight = height
+            };
+
+            ContentDialogResult result = await dialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                editor.Document.Selection.InsertImage((int)dialog.DefaultWidth, (int)dialog.DefaultHeight, 0, VerticalCharacterAlignment.Baseline, string.IsNullOrWhiteSpace(dialog.Tag) ? "Image" : dialog.Tag, randAccStream);
+                return;
+            }
+
+            // Insert an image
+            editor.Document.Selection.InsertImage(width, height, 0, VerticalCharacterAlignment.Baseline, "Image", randAccStream);
+        }
+    }
+    private void AddLinkButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (Windows.Foundation.Metadata.ApiInformation.IsPropertyPresent("Windows.UI.Xaml.FrameworkElement", "AllowFocusOnInteraction"))
+            hyperlinkText.AllowFocusOnInteraction = true;
+        editor.Document.Selection.Link = $"\"{hyperlinkText.Text}\"";
+        editor.Document.Selection.CharacterFormat.ForegroundColor = (Color)XamlBindingHelper.ConvertValue(typeof(Color), "#6194c7");
+        AddLinkButton.Flyout.Hide();
     }
 }
